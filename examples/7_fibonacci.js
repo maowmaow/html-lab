3bf6d09a422920c3b3d4884d98ffb915de1ebb5a
Montri M <montri.manatthon@thomsonreuters.com>
2014-10-28 18:46:54 +0700

    add more example

---
+++ examples/7_fibonacci.js
@@ -0,0 +1,21 @@
+
+function getFibonacci(n) {
+	var a = 0;
+	var b = 1;
+	var c = 0;
+	
+	if (n == 0) return 0;
+	for (var i=0; i<n; i++) {
+		c = a + b;
+		a = b;
+		b = c;
+	} 
+	return c;
+}
+
+self.addEventListener('message', function(e) {
+	console.log('finding fibonacci for ' + e.data);
+	var result = getFibonacci(e.data);
+	self.postMessage(result);
+	self.close();
+});
